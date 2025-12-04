f888c23dc63c37b16766fe7d7fe97ebd914efbc8
Wenri JUSHI <6704443+Wenri@users.noreply.github.com>
2025-12-04 19:33:42 +0100

    Convert ToC to ES module and fix import map scope
    - Convert table-of-contents.js to modern ES module syntax
    - Fix typo: elem.style.disply → elem.style.display
    - Move import map outside conditional block
    - Ensures import map is always available for lunr-search module

--- assets/js/plugins/table-of-contents.js
+++ assets/js/plugins/table-of-contents.js
@@ -5,43 +5,39 @@
  * Licensed MIT
  */
 
-var tocToggle = document.getElementById('toc-toggle');
-var tableOfContents = document.getElementById('markdown-toc');
+const tocToggle = document.getElementById('toc-toggle');
+const tableOfContents = document.getElementById('markdown-toc');
 
 function showToc(elem) {
-  function getHeight() {
+  const getHeight = () => {
     elem.style.display = 'block';
-
-    var height = elem.scrollHeight + 'px';
-
-    elem.style.disply = '';
-
+    const height = elem.scrollHeight + 'px';
+    elem.style.display = '';
     return height;
   };
 
-  var height = getHeight();
+  const height = getHeight();
   elem.classList.add('js-toc-is-open');
   elem.style.height = height;
 
-  window.setTimeout(function() {
+  setTimeout(() => {
     elem.style.height = '';
   }, 350);
-};
+}
 
 function hideToc(elem) {
   elem.style.height = elem.scrollHeight + 'px';
 
-  window.setTimeout(function() {
+  setTimeout(() => {
     elem.style.height = '0';
   }, 1);
 
-  window.setTimeout(function() {
+  setTimeout(() => {
     elem.classList.remove('js-toc-is-open');
   }, 350);
-};
+}
 
 function toggleToc(elem) {
-
   tocToggle.classList.toggle('js-toc-is-open');
 
   if (elem.classList.contains('js-toc-is-open')) {
@@ -50,10 +46,10 @@ function toggleToc(elem) {
   }
 
   showToc(elem);
-};
+}
 
 if (tocToggle) {
-  tocToggle.addEventListener("click", function(event) {
+  tocToggle.addEventListener('click', () => {
     toggleToc(tableOfContents);
   });
 }
